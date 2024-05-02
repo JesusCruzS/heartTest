e9b9e1c5f7943a4494d53f76d4e8c79a19791a44
Jesus Antonio Cruz Suarez <jesus.cruz@alternova.com>
2024-05-02 08:36:55 -0500

    Initial Commit

---
+++ stepsApp/stepsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepsAppApp.swift
+//  stepsApp
+//
+//  Created by Jesus Cruz Suárez on 2/05/24.
+//
+
+import SwiftUI
+
+@main
+struct stepsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
